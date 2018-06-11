fea8691db3d89288198d8cc3b44c2c825a76b212
David Tumpowsky <tumpowskydavid@gmail.com>
2018-06-11 16:08:44 -0700

    access album detail using firebase keys

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Album } from '../album.model';
 import { AlbumService } from '../album.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-album-detail',
@@ -12,19 +13,15 @@ import { AlbumService } from '../album.service';
 })
 
 export class AlbumDetailComponent implements OnInit {
-  albumId: number = null;
-  albumToDisplay: Album;
+  albumId: string;
+  albumToDisplay;
 
+  constructor(private route: ActivatedRoute, private location: Location, private albumService: AlbumService) { }
 
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location,
-    private albumService: AlbumService) {}
-
-  ngOnInit(){
+  ngOnInit() {
     this.route.params.forEach((urlParameters) => {
-      this.albumId = parseInt(urlParameters['id']);
-    });
-    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
-  }
+     this.albumId = urlParameters['id'];
+   });
+   this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
+ }
 }
--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+// import { ALBUMS } from './mock-albums';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
@@ -20,11 +20,7 @@ export class AlbumService {
     return this.albums;
   }
 
-  // getAlbumById(albumId: number){
-  //   for (var i = 0; i <= ALBUMS.length - 1; i++) {
-  //     if (ALBUMS[i].id === albumId) {
-  //       return ALBUMS[i];
-  //     }
-  //   }
-  // }
+  getAlbumById(albumId: string){
+    return this.database.object('albums/' + albumId);
+  }
 }
